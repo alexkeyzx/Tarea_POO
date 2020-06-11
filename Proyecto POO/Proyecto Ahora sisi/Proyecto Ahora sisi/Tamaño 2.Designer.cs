32aa49511b8ea44a05b04daaf97ad94496b03057
alexkeyzx <66797315+alexkeyzx@users.noreply.github.com>
2020-06-11 13:28:26 -0500

    Add files via upload

---
+++ Proyecto POO/Proyecto Ahora sisi/Proyecto Ahora sisi/Tamaño 2.Designer.cs
@@ -0,0 +1,333 @@
+﻿namespace Proyecto_Ahora_sisi
+{
+    partial class Tamaño_2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox13 = new System.Windows.Forms.PictureBox();
+            this.pictureBox14 = new System.Windows.Forms.PictureBox();
+            this.pictureBox15 = new System.Windows.Forms.PictureBox();
+            this.pictureBox16 = new System.Windows.Forms.PictureBox();
+            this.pictureBox9 = new System.Windows.Forms.PictureBox();
+            this.pictureBox10 = new System.Windows.Forms.PictureBox();
+            this.pictureBox11 = new System.Windows.Forms.PictureBox();
+            this.pictureBox12 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox17 = new System.Windows.Forms.PictureBox();
+            this.btnTomarFoto = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox13)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox14)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox15)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox16)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox11)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox12)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox17)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(265, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 13);
+            this.label1.TabIndex = 200;
+            this.label1.Text = "Nombre de archivo";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(268, 32);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(109, 20);
+            this.txtNombre.TabIndex = 199;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.button1.Location = new System.Drawing.Point(268, 58);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 29);
+            this.button1.TabIndex = 198;
+            this.button1.Text = "Randomizar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox13
+            // 
+            this.pictureBox13.Location = new System.Drawing.Point(192, 284);
+            this.pictureBox13.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.pictureBox13.Name = "pictureBox13";
+            this.pictureBox13.Size = new System.Drawing.Size(61, 92);
+            this.pictureBox13.TabIndex = 195;
+            this.pictureBox13.TabStop = false;
+            // 
+            // pictureBox14
+            // 
+            this.pictureBox14.Location = new System.Drawing.Point(72, 284);
+            this.pictureBox14.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.pictureBox14.Name = "pictureBox14";
+            this.pictureBox14.Size = new System.Drawing.Size(61, 92);
+            this.pictureBox14.TabIndex = 194;
+            this.pictureBox14.TabStop = false;
+            // 
+            // pictureBox15
+            // 
+            this.pictureBox15.Location = new System.Drawing.Point(132, 284);
+            this.pictureBox15.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.pictureBox15.Name = "pictureBox15";
+            this.pictureBox15.Size = new System.Drawing.Size(61, 92);
+            this.pictureBox15.TabIndex = 193;
+            this.pictureBox15.TabStop = false;
+            // 
+            // pictureBox16
+            // 
+            this.pictureBox16.Location = new System.Drawing.Point(12, 284);
+            this.pictureBox16.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.pictureBox16.Name = "pictureBox16";
+            this.pictureBox16.Size = new System.Drawing.Size(61, 92);
+            this.pictureBox16.TabIndex = 192;
+            this.pictureBox16.TabStop = false;
+            // 
+            // pictureBox9
+            // 
+            this.pictureBox9.Location = new System.Drawing.Point(192, 194);
+            this.pictureBox9.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.pictureBox9.Name = "pictureBox9";
+            this.pictureBox9.Size = new System.Drawing.Size(61, 92);
+            this.pictureBox9.TabIndex = 191;
+            this.pictureBox9.TabStop = false;
+            // 
+            // pictureBox10
+            // 
+            this.pictureBox10.Location = new System.Drawing.Point(132, 194);
+            this.pictureBox10.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.pictureBox10.Name = "pictureBox10";
+            this.pictureBox10.Size = new System.Drawing.Size(61, 92);
+            this.pictureBox10.TabIndex = 190;
+            this.pictureBox10.TabStop = false;
+            // 
+            // pictureBox11
+            // 
+            this.pictureBox11.Location = new System.Drawing.Point(72, 194);
+            this.pictureBox11.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.pictureBox11.Name = "pictureBox11";
+            this.pictureBox11.Size = new System.Drawing.Size(61, 92);
+            this.pictureBox11.TabIndex = 189;
+            this.pictureBox11.TabStop = false;
+            // 
+            // pictureBox12
+            // 
+            this.pictureBox12.Location = new System.Drawing.Point(12, 194);
+            this.pictureBox12.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.pictureBox12.Name = "pictureBox12";
+            this.pictureBox12.Size = new System.Drawing.Size(61, 92);
+            this.pictureBox12.TabIndex = 188;
+            this.pictureBox12.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Location = new System.Drawing.Point(192, 104);
+            this.pictureBox5.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(61, 92);
+            this.pictureBox5.TabIndex = 187;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Location = new System.Drawing.Point(132, 104);
+            this.pictureBox6.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(61, 92);
+            this.pictureBox6.TabIndex = 186;
+            this.pictureBox6.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.Location = new System.Drawing.Point(72, 104);
+            this.pictureBox7.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(61, 92);
+            this.pictureBox7.TabIndex = 185;
+            this.pictureBox7.TabStop = false;
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.Location = new System.Drawing.Point(12, 104);
+            this.pictureBox8.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(61, 92);
+            this.pictureBox8.TabIndex = 184;
+            this.pictureBox8.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(192, 14);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(61, 92);
+            this.pictureBox3.TabIndex = 183;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(132, 14);
+            this.pictureBox4.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(61, 92);
+            this.pictureBox4.TabIndex = 182;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(72, 14);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(61, 92);
+            this.pictureBox2.TabIndex = 181;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 14);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(61, 92);
+            this.pictureBox1.TabIndex = 180;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox17
+            // 
+            this.pictureBox17.Location = new System.Drawing.Point(12, 15);
+            this.pictureBox17.Name = "pictureBox17";
+            this.pictureBox17.Size = new System.Drawing.Size(241, 363);
+            this.pictureBox17.TabIndex = 196;
+            this.pictureBox17.TabStop = false;
+            // 
+            // btnTomarFoto
+            // 
+            this.btnTomarFoto.Location = new System.Drawing.Point(268, 93);
+            this.btnTomarFoto.Name = "btnTomarFoto";
+            this.btnTomarFoto.Size = new System.Drawing.Size(109, 28);
+            this.btnTomarFoto.TabIndex = 253;
+            this.btnTomarFoto.Text = "Tomar foto";
+            this.btnTomarFoto.UseVisualStyleBackColor = true;
+            this.btnTomarFoto.Click += new System.EventHandler(this.Foto_Click);
+            // 
+            // Tamaño_2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(398, 383);
+            this.Controls.Add(this.btnTomarFoto);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox13);
+            this.Controls.Add(this.pictureBox14);
+            this.Controls.Add(this.pictureBox15);
+            this.Controls.Add(this.pictureBox16);
+            this.Controls.Add(this.pictureBox9);
+            this.Controls.Add(this.pictureBox10);
+            this.Controls.Add(this.pictureBox11);
+            this.Controls.Add(this.pictureBox12);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.pictureBox8);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox17);
+            this.Name = "Tamaño_2";
+            this.Text = "Tamaño_2";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox13)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox14)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox15)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox16)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox11)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox12)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox17)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox13;
+        private System.Windows.Forms.PictureBox pictureBox14;
+        private System.Windows.Forms.PictureBox pictureBox15;
+        private System.Windows.Forms.PictureBox pictureBox16;
+        private System.Windows.Forms.PictureBox pictureBox9;
+        private System.Windows.Forms.PictureBox pictureBox10;
+        private System.Windows.Forms.PictureBox pictureBox11;
+        private System.Windows.Forms.PictureBox pictureBox12;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox17;
+        private System.Windows.Forms.Button btnTomarFoto;
+    }
+}
---
+++ Proyecto POO/Proyecto Ahora sisi/Proyecto Ahora sisi/Tamaño 2.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Proyecto_Ahora_sisi
+{
+    
+    public partial class Tamaño_2 : Form
+    {
+        Carta carta = new Carta();
+        public Tamaño_2()
+        {
+            InitializeComponent();
+            carta.Guardado += MetodoGestor;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            //codigo nombramiento de fotos
+            string[] Fotos = new string[55];
+            Fotos[0] = "C:\\Barajas\\Tamaño 2\\1.jpg";
+            Fotos[1] = "C:\\Barajas\\Tamaño 2\\2.jpg";
+            Fotos[2] = "C:\\Barajas\\Tamaño 2\\3.jpg";
+            Fotos[3] = "C:\\Barajas\\Tamaño 2\\4.jpg";
+            Fotos[4] = "C:\\Barajas\\Tamaño 2\\5.jpg";
+            Fotos[5] = "C:\\Barajas\\Tamaño 2\\6.jpg";
+            Fotos[6] = "C:\\Barajas\\Tamaño 2\\7.jpg";
+            Fotos[7] = "C:\\Barajas\\Tamaño 2\\8.jpg";
+            Fotos[8] = "C:\\Barajas\\Tamaño 2\\9.jpg";
+            Fotos[9] = "C:\\Barajas\\Tamaño 2\\10.jpg";
+            Fotos[10] = "C:\\Barajas\\Tamaño 2\\11.jpg";
+            Fotos[11] = "C:\\Barajas\\Tamaño 2\\12.jpg";
+            Fotos[12] = "C:\\Barajas\\Tamaño 2\\13.jpg";
+            Fotos[13] = "C:\\Barajas\\Tamaño 2\\14.jpg";
+            Fotos[14] = "C:\\Barajas\\Tamaño 2\\15.jpg";
+            Fotos[15] = "C:\\Barajas\\Tamaño 2\\16.jpg";
+            Fotos[16] = "C:\\Barajas\\Tamaño 2\\17.jpg";
+            Fotos[17] = "C:\\Barajas\\Tamaño 2\\18.jpg";
+            Fotos[18] = "C:\\Barajas\\Tamaño 2\\19.jpg";
+            Fotos[19] = "C:\\Barajas\\Tamaño 2\\20.jpg";
+            Fotos[20] = "C:\\Barajas\\Tamaño 2\\21.jpg";
+            Fotos[21] = "C:\\Barajas\\Tamaño 2\\22.jpg";
+            Fotos[22] = "C:\\Barajas\\Tamaño 2\\23.jpg";
+            Fotos[23] = "C:\\Barajas\\Tamaño 2\\24.jpg";
+            Fotos[24] = "C:\\Barajas\\Tamaño 2\\25.jpg";
+            Fotos[25] = "C:\\Barajas\\Tamaño 2\\26.jpg";
+            Fotos[26] = "C:\\Barajas\\Tamaño 2\\27.jpg";
+            Fotos[27] = "C:\\Barajas\\Tamaño 2\\28.jpg";
+            Fotos[28] = "C:\\Barajas\\Tamaño 2\\29.jpg";
+            Fotos[29] = "C:\\Barajas\\Tamaño 2\\30.jpg";
+            Fotos[30] = "C:\\Barajas\\Tamaño 2\\31.jpg";
+            Fotos[31] = "C:\\Barajas\\Tamaño 2\\32.jpg";
+            Fotos[32] = "C:\\Barajas\\Tamaño 2\\33.jpg";
+            Fotos[33] = "C:\\Barajas\\Tamaño 2\\34.jpg";
+            Fotos[34] = "C:\\Barajas\\Tamaño 2\\35.jpg";
+            Fotos[35] = "C:\\Barajas\\Tamaño 2\\36.jpg";
+            Fotos[36] = "C:\\Barajas\\Tamaño 2\\37.jpg";
+            Fotos[37] = "C:\\Barajas\\Tamaño 2\\38.jpg";
+            Fotos[38] = "C:\\Barajas\\Tamaño 2\\39.jpg";
+            Fotos[39] = "C:\\Barajas\\Tamaño 2\\40.jpg";
+            Fotos[40] = "C:\\Barajas\\Tamaño 2\\41.jpg";
+            Fotos[41] = "C:\\Barajas\\Tamaño 2\\42.jpg";
+            Fotos[42] = "C:\\Barajas\\Tamaño 2\\43.jpg";
+            Fotos[43] = "C:\\Barajas\\Tamaño 2\\44.jpg";
+            Fotos[44] = "C:\\Barajas\\Tamaño 2\\45.jpg";
+            Fotos[45] = "C:\\Barajas\\Tamaño 2\\46.jpg";
+            Fotos[46] = "C:\\Barajas\\Tamaño 2\\47.jpg";
+            Fotos[47] = "C:\\Barajas\\Tamaño 2\\48.jpg";
+            Fotos[48] = "C:\\Barajas\\Tamaño 2\\49.jpg";
+            Fotos[49] = "C:\\Barajas\\Tamaño 2\\50.jpg";
+            Fotos[50] = "C:\\Barajas\\Tamaño 2\\51.jpg";
+            Fotos[51] = "C:\\Barajas\\Tamaño 2\\52.jpg";
+            Fotos[52] = "C:\\Barajas\\Tamaño 2\\53.jpg";
+            Fotos[53] = "C:\\Barajas\\Tamaño 2\\54.jpg";
+
+          
+                //codigo cambiar imagenes
+                Random random = new Random();
+                int[] numeros = new int[16];
+                numeros[0] = -2;
+                numeros[1] = -2;
+                numeros[2] = -2;
+                numeros[3] = -2;
+                numeros[4] = -2;
+                numeros[5] = -2;
+                numeros[6] = -2;
+                numeros[7] = -2;
+                numeros[8] = -2;
+                numeros[9] = -2;
+                numeros[10] = -2;
+                numeros[11] = -2;
+                numeros[12] = -2;
+                numeros[13] = -2;
+                numeros[14] = -2;
+                numeros[15] = -2;
+
+
+                for (int i = 0; i < 16; i++)
+                {
+                    int n = random.Next(0, 54);
+                    //evita que se repitan fotos
+
+
+                    //generar(1) from[matriz principal]
+                    //genera el 45, entonces//  put(44),(46) on[matriz relevo]
+
+                    //generar(1) from[matriz principal]
+                    //genera el 2, entonces// put(1),(3) on[matriz relevo]
+                    int na, nb;
+                    na = n + 1;
+                    nb = n - 1;
+                    if (numeros.Contains(n) == !true)
+                    {
+
+                        if (numeros.Contains(na) == true || numeros.Contains(nb) == true)
+                        {
+                            i--;
+                            continue;
+                        }
+                       
+                        numeros[i] = n;
+                        continue;
+                    }
+                    else
+                    {
+                        i--;
+                    }
+
+
+                }
+
+                Image image = Image.FromFile(Fotos[numeros[0]]);
+                pictureBox1.Image = image;
+                image = Image.FromFile(Fotos[numeros[1]]);
+                pictureBox2.Image = image;
+                image = Image.FromFile(Fotos[numeros[2]]);
+                pictureBox3.Image = image;
+                image = Image.FromFile(Fotos[numeros[3]]);
+                pictureBox4.Image = image;
+                image = Image.FromFile(Fotos[numeros[4]]);
+                pictureBox5.Image = image;
+                image = Image.FromFile(Fotos[numeros[5]]);
+                pictureBox6.Image = image;
+                image = Image.FromFile(Fotos[numeros[6]]);
+                pictureBox7.Image = image;
+                image = Image.FromFile(Fotos[numeros[7]]);
+                pictureBox8.Image = image;
+                image = Image.FromFile(Fotos[numeros[8]]);
+                pictureBox9.Image = image;
+                image = Image.FromFile(Fotos[numeros[9]]);
+                pictureBox10.Image = image;
+                image = Image.FromFile(Fotos[numeros[10]]);
+                pictureBox11.Image = image;
+                image = Image.FromFile(Fotos[numeros[11]]);
+                pictureBox12.Image = image;
+                image = Image.FromFile(Fotos[numeros[12]]);
+                pictureBox13.Image = image;
+                image = Image.FromFile(Fotos[numeros[13]]);
+                pictureBox14.Image = image;
+                image = Image.FromFile(Fotos[numeros[14]]);
+                pictureBox15.Image = image;
+                image = Image.FromFile(Fotos[numeros[15]]);
+                pictureBox16.Image = image;
+
+            
+        }
+       
+      
+    
+        private void Foto_Click(object sender, EventArgs e)
+        {
+            carta.intLimites = pictureBox17.Size;
+            carta.strNombre = txtNombre.Text;
+            carta.intX = pictureBox17.Bounds.X;
+            carta.intY = pictureBox17.Bounds.Y;
+            Rectangle bounds = this.Bounds;
+            Bitmap bitmap = new Bitmap(bounds.Width, bounds.Height);
+            
+            carta.guardar(bitmap, bounds);
+            
+            //carta.guardar(bitmap,bounds);
+
+
+        }
+        public void MetodoGestor(Bitmap bitmap, Rectangle bounds)
+        {
+            MessageBox.Show("Se termino de almacenar el documento en: C:\\Barajas");
+        }
+    }
+    }
+
